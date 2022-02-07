98c48b1ae8909d8f5963b8498379aecbd7123368
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2022-02-07 07:46:12 -0500

    new api. fetch coins list

--- src/App.js
+++ src/App.js
@@ -1,45 +1,79 @@
-//I'm missing the images and the full name of the assets only have the abriviation and the usd rate.
-//the abriviation is the property name in live and the object name in list
-//I have the images and the full name in the list endpoint how can I combine both?
-//find a different API
-import React, { Component } from 'react';
-console.log(`${process.env.REACT_APP_COINASSETS_API_KEY}`) 
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-class App extends Component {
-  state={
-    baseURL: 'http://api.coinlayer.com/api/',
-    endPoint1: 'live',
-    endPoint2: 'list',
-    apiKey: `?access_key=${process.env.REACT_APP_COINASSETS_API_KEY}`,
-    coinsListURL: '',
-    coinsList: {}    
-  }
-  componentDidMount(){
-    this.setState({
-      coinsListURL: this.state.baseURL+this.state.endPoint2+this.state.apiKey
-    }, ()=>{
-      fetch(this.state.coinsListURL)
-      .then(res=>res.json())
-      .then(data=>this.setState({
-        coinsList: data.crypto[611].name
-      }))
-      .catch(error=>console.error(error))
-    })
-  } 
+function App() {
+  const [data, setData]=useState([])
+  const [userInput, setUserInput]=useState('')
 
-  render() {
-    return (
-      <div>
-        <a href={this.state.coinsListURL}>{this.state.coinsListURL}</a>
-        {console.log(this.state.coinsList)}
-        {/* <ul>
-        {this.state.coinsList.map((coin)=>{
-          <li>{coin}</li>
-        })}
-        </ul> */}
-      </div>
-    );
+  // useEffect(()=>{
+  //   axios.get('https://api.coinstats.app/public/v1/coins?skip=0&limit=100%C2%A4cy=INR')
+  //   .then(res=>setData(res.data.coins))
+  // })
+
+  useEffect(() => {
+    axios.get('https://api.coinstats.app/public/v1/coins?skip=0&limit=100¤cy=INR'
+    ).then((res) =>
+      setData(res.data.coins));
+  }, []);
+  
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log('searching for', userInput);
   }
+  const handleChange=e=>{
+    setUserInput(e.target.value)
+  }
+
+  return (
+    <div>
+      {console.log(data)}
+      <h1>Trade</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" 
+        id="userInput"
+        onChange={handleChange}
+        value={userInput}
+        />
+        <button>Search</button>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Symbol</th>
+            <th>Market Cap</th>
+            <th>Price</th>
+            <th>Available Supply</th>
+            <th>Volume 24hrs</th>
+            <th>Watch</th>
+          </tr>
+            {data.map((coin, id)=>{
+              return(
+                <tr key={id}>
+                  <td>{coin.rank}</td>
+                  <td>
+                    <a href={coin.websiteUrl}><img src={coin.icon} alt="coin logo" width="30px" /></a>
+                    <span>{coin.name}</span>
+                  </td>
+                  <td>{coin.symbol}</td>
+                  <td>{((Math.round(coin.marketCap)))}</td>
+                  <td>{coin.price.toLocaleString('en-US',{
+                    style: 'currency',
+                    currency: 'USD'
+                  })}</td>
+                  <td>{(Math.round(coin.availableSupply)).toLocaleString()}</td>
+                  <td>{(Math.round(coin.volume)).toLocaleString()}</td>
+                </tr>
+
+              )
+            })}
+      
+        </thead>
+      </table>
+    </div>
+  );
 }
 
 export default App;
+
