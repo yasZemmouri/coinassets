03623b9f1b849a1a20880d728ff9637c79481cf1
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2022-02-07 12:06:14 -0500

    coin not found message

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
       console.log('searching for', userInput);
       axios.get(`https://api.coinstats.app/public/v1/coins/${userInput.trim().toLowerCase().replace(' ', '-')}?currency=USD`)
       .then(res=>setCoinsList([res.data.coin]))
+      .catch(err=>console.log(err))
     }
     setUserInput('')
   }
@@ -29,7 +30,6 @@ function App() {
   return (
     <div>
       {console.log(coinsList)}
-      {/* {console.log(coinsList)} */}
       <h1>Trade</h1>
       <form onSubmit={handleSubmit}>
         <input type="text" 
@@ -39,6 +39,7 @@ function App() {
         />
         <button>Search</button>
       </form>
+      {(coinsList[0] !==undefined) ?
       <table>
         <thead>
           <tr>
@@ -52,8 +53,7 @@ function App() {
             <th>Watch</th>
           </tr>
         </thead>
-        <tbody>
-
+        <tbody>       
             {coinsList.map((coin, id)=>{
               return(
                 <tr key={id}>
@@ -72,17 +72,22 @@ function App() {
                   <td>{(Math.round(coin.volume)).toLocaleString()}</td>
                   <td>
                     <button aria-label="add to watchlist" aria-selected="true">
-        <svg hight="23" width="24" viewBox="0 0 24 23" filled="1" fill="none" focusable="false" aria-hidden='true' >
-            <path d="M12.713 1.443l2.969 6.015 6.637.965a.794.794 0 01.44 1.354l-4.804 4.681 1.135 6.612a.794.794 0 01-1.152.837L12 18.787l-5.938 3.121a.795.795 0 01-1.152-.838l1.134-6.612L1.24 9.777a.794.794 0 01.44-1.354l6.638-.965 2.968-6.015a.795.795 0 011.425 0z" ></path>
-        </svg>
-    </button></td>
+                      <svg hight="23" width="24" viewBox="0 0 24 23" filled="1" fill="none" focusable="false" aria-hidden='true' >
+                        <path d="M12.713 1.443l2.969 6.015 6.637.965a.794.794 0 01.44 1.354l-4.804 4.681 1.135 6.612a.794.794 0 01-1.152.837L12 18.787l-5.938 3.121a.795.795 0 01-1.152-.838l1.134-6.612L1.24 9.777a.794.794 0 01.44-1.354l6.638-.965 2.968-6.015a.795.795 0 011.425 0z" ></path>
+                      </svg>
+                    </button>
+                  </td>
                 </tr>
 
               )
-            })}
-      
+            }
+            
+          )
+          }
         </tbody>
       </table>
+      :<h2>Coin not found</h2>
+      }
     </div>
   );
 }
