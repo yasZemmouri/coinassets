6d71d130b805e5beaed20467c345ea118e172f7d
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2022-02-07 09:04:09 -0500

    add a star button for watch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './App.css'
 
 function App() {
   const [coinsList, setCoinsList]=useState([])
@@ -69,6 +70,12 @@ function App() {
                   })}</td>
                   <td>{(Math.round(coin.availableSupply)).toLocaleString()}</td>
                   <td>{(Math.round(coin.volume)).toLocaleString()}</td>
+                  <td>
+                    <button aria-label="add to watchlist" aria-selected="true">
+        <svg hight="23" width="24" viewBox="0 0 24 23" filled="1" fill="none" focusable="false" aria-hidden='true' >
+            <path d="M12.713 1.443l2.969 6.015 6.637.965a.794.794 0 01.44 1.354l-4.804 4.681 1.135 6.612a.794.794 0 01-1.152.837L12 18.787l-5.938 3.121a.795.795 0 01-1.152-.838l1.134-6.612L1.24 9.777a.794.794 0 01.44-1.354l6.638-.965 2.968-6.015a.795.795 0 011.425 0z" ></path>
+        </svg>
+    </button></td>
                 </tr>
 
               )
