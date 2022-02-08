b7399a961b66fa51a0f704f9eb6a1d1fee694cc9
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2022-02-07 21:06:22 -0500

    loading anim, components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
+import CoinData from './components/CoinData';
 import './App.css'
+
 
 function App() {
   const [coinsList, setCoinsList]=useState([])
   const [userInput, setUserInput]=useState('')
+  const [loading, setLoading]=useState(false)
   // const [searchRes, setSearchRes]=useState({})
 
   useEffect(() => {
+    setLoading(true)
     axios.get('https://api.coinstats.app/public/v1/coins?skip=0&limit=100&curreny=USD'
-    ).then((res) =>
-      setCoinsList(res.data.coins));
+    ).then((res) =>{
+        setCoinsList(res.data.coins)
+        setLoading(false)
+      }
+      )
   }, []);
   
   const handleSubmit=(e)=>{
     e.preventDefault();
+    setLoading(true);
     if(userInput){
       console.log('searching for', userInput);
       axios.get(`https://api.coinstats.app/public/v1/coins/${userInput.trim().toLowerCase().replace(' ', '-')}?currency=USD`)
-      .then(res=>setCoinsList([res.data.coin]))
+      .then(res=>{
+        setCoinsList([res.data.coin]);
+        setLoading(false)
+      })
       .catch(err=>console.log(err))
     }
     setUserInput('')
@@ -39,7 +50,8 @@ function App() {
         />
         <button>Search</button>
       </form>
-      {(coinsList[0] !==undefined) ?
+      {(loading &&  <img src='https://i.stack.imgur.com/hzk6C.gif' alt=''/>)|| 
+      (coinsList[0] !==undefined &&
       <table>
         <thead>
           <tr>
@@ -54,39 +66,17 @@ function App() {
           </tr>
         </thead>
         <tbody>       
-            {coinsList.map((coin, id)=>{
+            {coinsList.map((coin)=>{
               return(
-                <tr key={id}>
-                  <td>{coin.rank}</td>
-                  <td>
-                    <a href={coin.websiteUrl}><img src={coin.icon} alt="coin logo" width="30px" /></a>
-                    <span>{coin.name}</span>
-                  </td>
-                  <td>{coin.symbol}</td>
-                  <td>{((Math.round(coin.marketCap)))}</td>
-                  <td>{coin.price.toLocaleString('en-US',{
-                    style: 'currency',
-                    currency: 'USD'
-                  })}</td>
-                  <td>{(Math.round(coin.availableSupply)).toLocaleString()}</td>
-                  <td>{(Math.round(coin.volume)).toLocaleString()}</td>
-                  <td>
-                    <button aria-label="add to watchlist" aria-selected="true">
-                      <svg hight="23" width="24" viewBox="0 0 24 23" filled="1" fill="none" focusable="false" aria-hidden='true' >
-                        <path d="M12.713 1.443l2.969 6.015 6.637.965a.794.794 0 01.44 1.354l-4.804 4.681 1.135 6.612a.794.794 0 01-1.152.837L12 18.787l-5.938 3.121a.795.795 0 01-1.152-.838l1.134-6.612L1.24 9.777a.794.794 0 01.44-1.354l6.638-.965 2.968-6.015a.795.795 0 011.425 0z" ></path>
-                      </svg>
-                    </button>
-                  </td>
-                </tr>
-
+                <CoinData coin={coin}/>
               )
             }
             
           )
           }
         </tbody>
-      </table>
-      :<h2>Coin not found</h2>
+      </table>)||
+      <h2>Coin not found</h2>
       }
     </div>
   );
---
+++ src/components/CoinData.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import Watchbton from './WatchBtn';
+
+const CoinData = (props) => {
+    const[active, setActive]=useState(false)
+    console.log(props.coin.name);
+    return (
+           <tr key={props.coin.name}>
+              <td>{props.coin.rank}</td>
+              <td>
+                    <a href={props.coin.websiteUrl}><img src={props.coin.icon} alt="coin logo" width="30px" /></a>
+                  <span>{props.coin.name}</span>
+                  </td>
+                  <td>{props.coin.symbol}</td>
+                  <td>{props.coin.marketCap.toLocaleString('en-US',{
+                    style: 'currency',
+                    currency: 'USD'})}</td>
+                  <td>{props.coin.price.toLocaleString('en-US',{
+                    style: 'currency',
+                    currency: 'USD'
+                  })}</td>
+                  <td>{(Math.round(props.coin.availableSupply)).toLocaleString()}</td>
+                  <td>{(Math.round(props.coin.volume)).toLocaleString()}</td>
+                  <td>
+                    <Watchbton isActive={active} onClick={()=>setActive(!active)}/>
+                  </td>
+            </tr> 
+       
+    );
+}
+
+export default CoinData;
